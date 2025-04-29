2cda6f5b41a4f791308bcbdca23ed9ceb4a103b2
Brendan-S-1058 <enpassantNH@gmail.com>
2025-04-29 01:45:37 +0000

    first steps, checking connections with search.py

--- src/Search.py
+++ src/Search.py
@@ -7,7 +7,32 @@ import json
 import gspread
 
 def Main ():
-    result = {"message": f"Main"}
+    global Vault
+    global TeamList
+    global countb
+    export_import_data = ""
+    countd = 0
+    DOUBLE = True
+    input = sys.stdin.read()
+
+    for char in input:
+        if char != "," and DOUBLE == True:
+            DOUBLE = False
+        if countd == 1:
+            export_import_data = export_import_data + "\n"
+            if char == ",":
+                DOUBLE = True
+        countd = 0
+        if char == "/":
+            countd = 1
+        if DOUBLE == False:
+            export_import_data = export_import_data + char
+    
+    state = False
+    NewSheet (state)
+
+
+    result = {export_import_data}
     print(json.dumps(result))
 
 Main ()
