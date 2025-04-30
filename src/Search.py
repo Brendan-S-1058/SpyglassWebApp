2589024fe92b7acfe6157a11837828f10b4cdb26
Brendan-S-1058 <enpassantNH@gmail.com>
2025-04-30 20:37:48 +0000

    major changes to making seach.py and dataProcessed work

--- src/Search.py
+++ src/Search.py
@@ -6,14 +6,16 @@ import sys
 import json
 import gspread
 
+allData = []
+
 def Main ():
     global Vault
     global TeamList
-    global countb
     export_import_data = ""
     countd = 0
     DOUBLE = True
-    input = sys.stdin.read()
+    input = "1,1058,1,2,1,2,4,1,1,4,4,4,4,1,1,0,0,1,No comment/2,1058,1,2,1,0,4,1,1,4,4,7,4,0,1,0,0,1,No comment/"
+    #input = sys.stdin.read()
 
     for char in input:
         if char != "," and DOUBLE == True:
@@ -27,9 +29,84 @@ def Main ():
             countd = 1
         if DOUBLE == False:
             export_import_data = export_import_data + char
+    NewSheet(export_import_data)
 
+def NewSheet (datain):
+
+    DataValue = ""
+    dataList = []
     array = []
-    array.append(export_import_data)
-    print(json.dumps(array))
+    counta = 0
+    moreData = False
+    countc = 0
+    leftovers = ""
+ #   print (counta)
+
+
+    linebreak = False
+
+    for char in datain: 
+        if char != ',' and char != '/' and linebreak == False:
+                DataValue = DataValue + char
+        elif linebreak == True and char and char != "\n":
+            moreData = True
+            leftovers += char
+        elif linebreak == False and char != "\"" and char != "[" and char != "]":
+            counta = counta + 1
+            dataList.append(DataValue)
+            if counta == 1:
+                nmatch = DataValue
+            elif counta == 2:
+                nteam = DataValue
+            DataValue = ""
+            if char == '/':
+                linebreak = True
+
+    if len(dataList)>0:
+        dpresent = True
+    else:
+        dpresent = False
+
+    if dpresent == True:
+        
+        #auto point total
+        dataList.append((int(dataList[2])*3)+(int(dataList[3])*3)+(int(dataList[4])*4)+(int(dataList[5])*6)+(int(dataList[6])*7)+(int(dataList[7])*6)+(int(dataList[8])*4))
+        #tele point total
+        dataList.append((int(dataList[9])*2)+(int(dataList[10])*3)+(int(dataList[11])*4)+(int(dataList[12])*5)+(int(dataList[13])*6)+(int(dataList[14])*4)+(int(dataList[15])*2)+(int(dataList[16])*6)+(int(dataList[17])*12))
+        #total points scored that match
+        dataList.append(dataList[19]+dataList[20])
+        #score stations:
+        #coral total
+        dataList.append(int(dataList[3])*3+int(dataList[4])*4+int(dataList[5])*6+int(dataList[6])*7+int(dataList[9])*2+int(dataList[10])*3+int(dataList[11])*4+int(dataList[12])*5)
+        #algae total
+        dataList.append(int(dataList[7])*6+int(dataList[8])*4+int(dataList[13])*6+int(dataList[14])*4)
+        #location total 
+        dataList.append(int(dataList[2])*3+int(dataList[15])*2+int(dataList[16])*6+int(dataList[17])*12)
+            #coral%
+        if dataList [21]>0:
+            dataList.append(round(int(dataList[22])/int(dataList[21])*100))
+            #algae%
+            dataList.append(round(int(dataList[23])/int(dataList[21])*100))
+            #location%
+            dataList.append(round(int(dataList[24])/int(dataList[21])*100))
+        else:
+            dataList.append(0)
+            dataList.append(0)
+            dataList.append(0)
+
+    allData.append(dataList)
+
+    if moreData == True:
+        NewSheet(leftovers)
+    else:
+        for i in range (len(allData)):
+            matchS = ""
+            matchL = allData[i]
+            for ix in range (len(matchL)):
+                matchS += str(matchL[ix]) + ","
+            array.append(matchS)
+            
+            print (array)
+            #print (json.dumps(array))
 
 Main ()
