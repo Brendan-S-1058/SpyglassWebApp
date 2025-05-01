049ec43ba4b48c01acdc95f758b53ab0cd7520f3
Brendan-S-1058 <enpassantNH@gmail.com>
2025-05-01 18:23:37 +0000

    moved return name and added log in dataProcessed

--- src/Search.py
+++ src/Search.py
@@ -5,7 +5,7 @@ import os
 import sys
 import json
 
-
+array = []
 allData = []
 
 def Main ():
@@ -46,7 +46,6 @@ def NewSheet (datain):
 
     DataValue = ""
     dataList = []
-    array = []
     counta = 0
     moreData = False
     countc = 0
@@ -117,7 +116,7 @@ def NewSheet (datain):
                 matchS += str(matchL[ix]) + ","
             array.append(matchS)
             print ("array: " + str(array), file=sys.stderr)
-            print (['1,1058,1,2,1,2,4,1,1,4,4,4,4,1,1,0,0,1,No comment,63,78,141,106,20,15,75,14,11,', '100,1058,1,0,0,0,2,1,0,0,1,3,7,3,4,0,0,1,Pretty cool,23,96,119,64,40,15,54,34,13,'], file=sys.stdout)
 
 
 Main()
+print (['1,1058,1,2,1,2,4,1,1,4,4,4,4,1,1,0,0,1,No comment,63,78,141,106,20,15,75,14,11,', '100,1058,1,0,0,0,2,1,0,0,1,3,7,3,4,0,0,1,Pretty cool,23,96,119,64,40,15,54,34,13,'], file=sys.stdout)
