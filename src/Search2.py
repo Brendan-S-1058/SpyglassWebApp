1483d9830e4be1cba1d35fbd3f162498a956a0e9
Brendan-S-1058 <enpassantNH@gmail.com>
2025-05-03 21:41:22 +0000

    revert if fail

--- src/Search2.py
+++ src/Search2.py
@@ -2,7 +2,7 @@ import sys
 import json
 
 dataLists = []
-finArray = []
+finArray = ""
 
 def Main ():
     inputR = sys.stdin.read()
@@ -10,6 +10,7 @@ def Main ():
     #inputS = '1,1058,1,2,1,2,4,1,1,4,4,4,4,1,1,0,0,1,No comment,63,78,141,106,20,15,75,14,11,100,1058,1,0,0,0,2,1,0,0,1,3,7,3,4,0,0,1,Pretty cool,23,96,119,64,40,15,54,34,13'
     holdList = []
     hold = ""
+    global finArray
     commaCount = 0
     DOUBLE = True
     countd = 0
@@ -72,15 +73,15 @@ def Main ():
         holdMedI = holdMedL[0]
 
     ##//Max score, median score, average score, avg algae score, avg coral score, avg loc score, avg auto score, avg tele score, avg auto % total score, avg tele % total score
-    finArray.append(str(holdMax))
-    finArray.append(str(holdMedI))
-    finArray.append(str(holdTotal/len(dataLists)))
-    finArray.append(str(holdAlgae/len(dataLists)))
-    finArray.append(str(holdCoral/len(dataLists)))
-    finArray.append(str(holdLoc/len(dataLists)))
-    finArray.append(str(holdAuto/len(dataLists)))
-    finArray.append(str(holdTele/len(dataLists)))
-    finArray.append(str(((holdAuto/holdTotal)*1000)//10))
-    finArray.append(str(((holdTele/holdTotal)*1000)//10))
+    finArray+=(str(holdMax)) + ','
+    finArray+=(str(holdMedI)) + ','
+    finArray+=(str(holdTotal/len(dataLists))) + ','
+    finArray+=(str(holdAlgae/len(dataLists))) + ','
+    finArray+=(str(holdCoral/len(dataLists))) + ','
+    finArray+=(str(holdLoc/len(dataLists))) + ','
+    finArray+=(str(holdAuto/len(dataLists))) + ','
+    finArray+=(str(holdTele/len(dataLists))) + ','
+    finArray+=(str(((holdAuto/holdTotal)*1000)//10)) + ','
+    finArray+=(str(((holdTele/holdTotal)*1000)//10)) + ','
 Main ()
 print (json.dumps(finArray))
