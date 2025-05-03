ee79aeda5f056d316b7a5657e466c1f97a98e106
Brendan-S-1058 <enpassantNH@gmail.com>
2025-05-03 20:06:18 +0000

    added calls to implement  dataProcessed

--- src/Search2.py
+++ src/Search2.py
@@ -1,11 +1,19 @@
+import sys
+import json
 
+dataLists = []
+finArray = []
 
 def Main ():
     inputR = sys.stdin.read()
     inputS = str(json.loads(inputR))
-    dataLists = []
+    #inputS = '1,1058,1,2,1,2,4,1,1,4,4,4,4,1,1,0,0,1,No comment,63,78,141,106,20,15,75,14,11,100,1058,1,0,0,0,2,1,0,0,1,3,7,3,4,0,0,1,Pretty cool,23,96,119,64,40,15,54,34,13'
     holdList = []
     hold = ""
+    commaCount = 0
+    DOUBLE = True
+    countd = 0
+    export_import_data = ""
 
     for char in inputS:
         if char != "," and DOUBLE == True:
@@ -20,19 +28,60 @@ def Main ():
         if DOUBLE == False:
             export_import_data = export_import_data + char
 
+    export_import_data += ","
+
     for char in export_import_data:
-        if char != "," and char != "/" and char != "\n" and char != "\"":
+        if char != "," and char != "\n" and char != "\"":
             hold += char 
-        if char == ",":
+            #print (hold)
+        if char == "," and commaCount != 28:
             holdList.append(hold)
             hold = ""
-        elif char == "/":
+            commaCount += 1
+        if commaCount == 28:
             dataLists.append(holdList)
             holdList = []
-
-
-
+            commaCount = 0
     
+    holdMax = 0
+    holdMedL = []
+    holdMedI = 0
+    holdTotal = 0
+    holdAlgae = 0
+    holdCoral = 0
+    holdAuto = 0
+    holdTele = 0
+    holdLoc = 0 
+    for i in range (len(dataLists)):
+        holdMedL.append(int(dataLists[i][21]))
+        if (int(dataLists[i][21]) > holdMax):
+            holdMax = int(dataLists[i][21])
+        holdTotal += int(dataLists[i][21])
+        holdAlgae += int(dataLists[i][23])
+        holdCoral += int(dataLists[i][22])
+        holdAuto += int(dataLists[i][19])
+        holdTele += int(dataLists[i][20])
+        holdloc += int(dataLists[i][24])
+    holdMedL.sort()
+    while len(holdMedL)>2:
+        holdMedL.pop(0)
+        holdMedL.pop()
+    if len(holdMedL)==2:
+        holdMedI = (holdMedL[0]+holdMedL[1])/2
+    else:
+        holdMedI = holdMedL[0]
+
+    ##//Max score, median score, average score, avg algae score, avg coral score, avg loc score, avg auto score, avg tele score, avg auto % total score, avg tele % total score
+    finArray.append(holdMax)
+    finArray.append(holdMedI)
+    finArray.append(holdTotal/len(dataLists))
+    finArray.append(holdAlgae/len(dataLists))
+    finArray.append(holdCoral/len(dataLists))
+    finArray.append(holdloc/len(dataLists))
+    finArray.append(holdAuto/len(dataLists))
+    finArray.append(holdTele/len(dataLists))
+    finArray.append(holdAuto/holdTele)
+    finArray.append(holdTele/holdTele)
 
 Main ()
-print (json.dumps(arrayR))
+print (json.dumps(finArray))
