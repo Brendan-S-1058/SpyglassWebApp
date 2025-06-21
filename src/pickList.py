452a24c89d2628488e7978d699bb633bfa62acec
Brendan-S-1058 <enpassantNH@gmail.com>
2025-06-21 20:13:42 +0000

    fool 2 (electric boogaloo)

--- src/pickList.py
+++ src/pickList.py
@@ -23,8 +23,8 @@ def Public ():
 def Local (string):
     print ("inputI: " + str(string), file=sys.stderr)
 
-def SortAndReturn ():
-    return null 
+def SortAndReturn (rawData):
+    return rawData 
 '''
 1: figure out input - (-1,,1,1058,1,2,1,2,4,1,1,4,4,4,4,1,1,0,0,1,No comment/,100,1058,1,0,0,0,2,1,0,0,1,3,7,3,4,0,0,1,Pretty cool/,100,3467,1,0,0,0,1,1,0,0,1,3,3,3,4,0,0,1,Pretty cool/) or (1)
 2: figure out intended output
