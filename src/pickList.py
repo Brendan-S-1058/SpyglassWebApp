f43bbe8b98a22e74355fb6d2b87ddc9342f2b47d
Brendan-S-1058 <enpassantNH@gmail.com>
2025-06-21 19:07:33 +0000

    added info for local input

--- src/pickList.py
+++ src/pickList.py
@@ -26,7 +26,7 @@ def SortAndReturn ():
     return null 
 '''
 '''
-1: figure out input
+1: figure out input - (-1,,1,1058,1,2,1,2,4,1,1,4,4,4,4,1,1,0,0,1,No comment/,100,1058,1,0,0,0,2,1,0,0,1,3,7,3,4,0,0,1,Pretty cool/,100,3467,1,0,0,0,1,1,0,0,1,3,3,3,4,0,0,1,Pretty cool/) or (1)
 2: figure out intended output
 3: make a structure to convert input to output in both cases
 '''
