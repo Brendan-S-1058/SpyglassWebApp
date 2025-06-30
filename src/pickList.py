64e9e2bec280fbb95cfb28b58f547d0b940739a5
Brendan-S-1058 <enpassantNH@gmail.com>
2025-06-30 01:10:01 +0000

    added comment

--- src/pickList.py
+++ src/pickList.py
@@ -74,6 +74,7 @@ def Process (inputData):
 
 '''
 1: figure out input - (-1,,1,1058,1,2,1,2,4,1,1,4,4,4,4,1,1,0,0,1,No comment/,100,1058,1,0,0,0,2,1,0,0,1,3,7,3,4,0,0,1,Pretty cool/,100,3467,1,0,0,0,1,1,0,0,1,3,3,3,4,0,0,1,Pretty cool/) or (1  1058,1,1,2,1,2,4,1,1,4,4,4,4,1,1,0,0,1,No comment,,1058,100,1,0,0,0,2,1,0,0,1,3,7,3,4,0,0,1,Pretty cool,,3467,100,1,0,0,0,1,1,0,0,1,3,3,3,4,0,0,1,Pretty cool,,)
+1.5: double check input - I think you fixed the double comma thing already, but I can't test on sl
 2: figure out intended output - (start at avg points scored per team)
 3: make a structure to convert input to output in both cases
 4: make it sortable by multiple modes
