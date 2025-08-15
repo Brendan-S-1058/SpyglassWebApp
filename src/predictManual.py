682188d820cc8322740a735819bbbb9ebcb95f49
Brendan-S-1058 <enpassantNH@gmail.com>
2025-08-15 22:35:24 +0000

    really reverted rebalancing

--- src/predictManual.py
+++ src/predictManual.py
@@ -193,7 +193,7 @@ def cap (match):
 
     if (match[1] + match[7]) > coralavg - 1.5 and (match[1] + match[7]) < 1.5 + coralavg:
         if match[7] > 0.05:
-            match[7] += 0.4
+            match[7] += 0.3
     
     if (match[2] + match[8]) > coralavg - 1.5 and (match[2] + match[8]) < coralavg + 1.5:
         if match[8] > 0.05:
@@ -201,11 +201,11 @@ def cap (match):
     
     if (match[3] + match[9]) > coralavg - 1.5 and (match[3] + match[9]) < 1.5 + coralavg:
         if match[7] > 0.05:
-            match[7] += 0.4
+            match[7] += 0.3
     
     if (match[4] + match[10]) > coralavg - 1.5 and (match[4] + match[10]) < coralavg + 1.5:
         if match[10] > 0.05:
-            match[10] += 0.6
+            match[10] += 0.3
     
     increment = 0
     bar = 6 
