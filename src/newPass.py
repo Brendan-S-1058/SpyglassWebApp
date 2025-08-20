c48c8b610d566cc29a946a4d63d019c4e2fe4390
Brendan-S-1058 <enpassantNH@gmail.com>
2025-08-20 19:23:57 +0000

    okay, I gotta add way more code to even test if this works, but at least I should test if it breaks what already worked

--- src/newPass.py
+++ src/newPass.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import os
 
 def Main ():
     inputR = sys.stdin.read()
@@ -22,6 +23,12 @@ def Main ():
 
     with open ("public/data/testData.txt", 'w') as file:
         file.write (team + '/' + passw + ',')
+        file.close ()
+
+    teamFileStructure = 'public/data/Teams/' + str(team) + '/' str(team) + 'Graphs'
+    os.makedirs (teamFileStructure)
+    
+    with open ("public/data/Teams/" + str(team) + '/' + str(team) + 'Public.txt', 'x') as file:
         file.close ()
     
     print (json.dumps("newpassword: " + str(passw)))
