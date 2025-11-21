2dd656ecdcae033f8583d35e42c60b2c548dc7d6
Brendan-S-1058 <enpassantNH@gmail.com>
2025-11-21 16:58:10 +0000

    may have almost made functioning opr

--- src/opr.py
+++ src/opr.py
@@ -1,6 +1,6 @@
 import sys
 import json
-import numpy as notpy
+import numpy as np
 
 #this is going to be great
 #MTMx = MTs
@@ -53,7 +53,85 @@ for match in holdestList:
 sortedData = holdestList
 print ('sortedData: ' + str(sortedData), file=sys.stderr)
 
-print (json.dumps(holdestList))
+matchsInData = []
+for smatch in sortedData:
+    if smatch[0] not in matchsInData:
+        matchsInData.append(smatch[0])
+
+teamsInData = []
+for smatch in sortedData:
+    if smatch[1] not in teamsInData:
+        teamsInData.append(smatch[1])
+
+alliancesByMatch = {}
+for match in matchsInData:
+    count = 0
+    for smatch in sortedData:
+        if match == smatch[0]:
+            totalScore = ((int(float(match[2]))*3)+(int(float(match[3]))*3)+(int(float(match[4]))*4)+(int(float(match[5]))*6)+(int(float(match[6]))*7)+(int(float(match[7]))*6)+(int(float(match[8]))*4))+((int(float(match[9]))*2)+(int(float(match[10]))*3)+(int(float(match[11]))*4)+(int(float(match[12]))*5)+(int(float(match[13]))*6)+(int(float(match[14]))*4)+(int(float(match[15]))*2)+(int(float(match[16]))*6)+(int(float(match[17]))*12))
+            #TODO: ADD REAL ALLIANCE SEPARATIONS WITH NEW INPUT PAGE
+            if count == 0:
+                alliancesByMatch[str(match) + 'team' + 'b'] = [smatch[1]]
+                alliancesByMatch[str(match) + 'score' + 'b'] = totalScore
+            elif count < 3:
+                alliancesByMatch[str(match) + 'team' + 'b'].append(smatch[1])
+                alliancesByMatch[str(match) + 'score' + 'b'] = totalScore
+            elif count == 3:
+                alliancesByMatch[str(match) + 'team' + 'r'] = [smatch[1]]
+                alliancesByMatch[str(match) + 'score' + 'r'] = totalScore
+            elif count < 6:
+                alliancesByMatch[str(match) + 'team' + 'r'].append(smatch[1])
+                alliancesByMatch[str(match) + 'score' + 'r'] = totalScore
+            count += 1
+
+teamsMatrixPre = []
+scoresMatrixPre = []
+for match in matchsInData:
+    holdListT = []
+    onAlliance = 0
+    for i in range(len(teamsInData)):
+        if teamsInData[i] in alliancesByMatch[str(match)+'team'+'b']:
+            holdListT.append(1)
+            onAlliance += 1
+        else:
+            holdListT.append(0)
+    if onAlliance == 3:
+        teamsMatrixPre.append(holdListT)
+        scoresMatrixPre.append([alliancesByMatch[str(match)+'team'+'b']])
+    holdListT = []
+    onAlliance = 0
+    for i in range(len(teamsInData)):
+        if teamsInData[i] in alliancesByMatch[str(match)+'team'+'r']:
+            holdListT.append(1)
+            onAlliance += 1
+        else:
+            holdListT.append(0)
+    if onAlliance == 3:
+        teamsMatrixPre.append(holdListT)
+        scoresMatrixPre.append([alliancesByMatch[str(match)+'team'+'r']])
+
+print ('teamsMatrixPre: ' + str(teamsMatrixPre), file=sys.stderr)
+print ('scoresMatrixPre: ' + str(scoresMatrixPre), file=sys.stderr)
+
+
+teamsMatrix = np.array(teamsMatrixPre)
+scoresMatrix = np.array(scoresMatrixPre)
+
+Mtrans = notpy.transpose(teamsMatrix)
+
+coes = notpy.matmul(Mtrans, teamsMatrix)
+ans = notpy.matmul(Mtrans, scoresMatrix)
+
+fins = notpy.linalg.solve(coes, ans)
+
+finalList = []
+for i in range (len(teamsInData)):
+    finalList.append(str(teamsInData[i]) + ',' + str(fins[i][0]))
+    finalList.append('\n')
+
+print ('finalList: ' + str(finalList), file=sys.stderr)
+
+print (json.dumps(finalList))
 
 '''count = 0
 holdList = []
