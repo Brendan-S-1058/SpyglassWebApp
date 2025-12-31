b4c83c87bb2798263bd77aff65123670250c4ec8
Brendan-S-1058 <enpassantNH@gmail.com>
2025-12-31 04:32:08 +0000

    hardcoded lists to ensure

--- src/oprPicklist.py
+++ src/oprPicklist.py
@@ -224,7 +224,7 @@ def Order (autolists, telelists, teamDir):
                 ys.append(i2[0])
 
 
-    plt.plot(xs, ys, marker='o', linestyle='None')
+    plt.plot([0,1,2,3,4,5,6,7,8,9,0,9,8,7,6,5,4,3,2,1], [0,1,2,3,4,5,6,7,8,9,0,9,8,7,6,5,4,3,2,1], marker='o', linestyle='None')
     plt.xlabel('Auto Average Points')
     plt.ylabel('Tele Average Points')
     for i in labels:
