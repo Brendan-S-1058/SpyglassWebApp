ea1757a9a6b52bbc1ff0857b91558246bddce862
Brendan-S-1058 <enpassantNH@gmail.com>
2025-12-31 04:02:15 +0000

    cool so that actually worked, and it makes sense now because I just had different nesting on the def and call

--- src/oprPicklist.py
+++ src/oprPicklist.py
@@ -183,14 +183,14 @@ def calcCopr(teamsInData, matchsInData, alliancesByMatch):
     sortingDict = {}
     keyList = []
     for i in range (len(teamsInData)):
-        sortingDict[fins[0][i][0]+int(teamsInData[i])/50000] = teamsInData[i]
-        keyList.append(fins[0][i][0]+int(teamsInData[i]/50000))
+        sortingDict[fins[0][i][0]+teamsInData[i]/100000] = teamsInData[i]
+        keyList.append(fins[0][i][0]+teamsInData[i]/100000)
 
     keyList.sort(reverse=True)
 
     finalList = []
     for i in range (len(keyList)):
-        finalList.append([(sortingDict[keyList[i]]), str(keyList[i]-int(sortingDict[keyList[i]])/50000)])
+        finalList.append([(sortingDict[keyList[i]]), str(keyList[i]-sortingDict[keyList[i]]/100000)])
 
     print ('finalList: ' + str(finalList), file=sys.stderr)
     return finalList
