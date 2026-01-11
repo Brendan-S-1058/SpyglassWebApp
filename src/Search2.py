0da7f806b8d152595369359d35cb3da0dc578c2b
Brendan-S-1058 <enpassantNH@gmail.com>
2026-01-11 14:39:11 +0000

    next (?) verse same as the first, a lot louder and a whole lot worse

--- src/Search2.py
+++ src/Search2.py
@@ -82,6 +82,22 @@ def Main ():
         holdMedI = (holdMedL[0]+holdMedL[1])/2
     else:
         holdMedI = holdMedL[0]
+    
+
+    print ('holdMax: ' + str(holdMax), file=sys.stderr)
+    print ('holdMedL: ' + str(holdMedL), file=sys.stderr)
+    print ('holdMedI: ' + str(holdMedI), file=sys.stderr)
+    print ('holdTotal: ' + str(holdTotal), file=sys.stderr)
+    print ('holdAClimb: ' + str(holdAClimb), file=sys.stderr)
+    print ('holdTClimb: ' + str(holdTClimb), file=sys.stderr)
+    print ('holdClimb: ' + str(holdClimb), file=sys.stderr)
+    print ('holdAfuel: ' + str(holdAfuel), file=sys.stderr)
+    print ('holdFuel: ' + str(holdFuel), file=sys.stderr)
+    print ('holdAuto: ' + str(holdAuto), file=sys.stderr)
+    print ('holdAcc: ' + str(holdAcc), file=sys.stderr)
+    print ('holdTele: ' + str(holdTele), file=sys.stderr)
+
+
     ##Maxscore, Medianscore, Averagescore, Accuracy, AutoClimbconsistency, teleClimbconsistency, teleclimbheight, autoavg, Afuel, teleavg, fuelper, climbper, autoTavg, teleTavg
     ##//Max score, median score, average score, accuracy, auto climb consistency, tele climb consisteny, tele climb height, avg auto score, avg auto fuel, tele avg score, avg fuel percent score, avg climb percent score, avg auto percent score, avg tele percent score
     finArray+=(str(holdMax)) + ','
@@ -98,5 +114,19 @@ def Main ():
     finArray+=(str((1000*holdClimb/holdTotal)//10)) + ','
     finArray+=(str(((1000*holdAuto/holdTotal))//10)) + ','
     finArray+=(str(((1000*holdTele/holdTotal))//10))
+
+    print ('str(holdMax): ' + str(holdMax), file=sys.stderr)
+    print ('str(holdMedI): ' + str(holdMedI), file=sys.stderr)
+    print ('str(holdTotal/len(dataLists): ' + str(holdTotal/len(dataLists)), file=sys.stderr)
+    print ('str(holdAcc/len(dataLists)): ' + str(holdAcc/len(dataLists)), file=sys.stderr)
+    print ('holdAClimb/len(dataLists): ' + str(holdAClimb/len(dataLists)), file=sys.stderr)
+    print ('holdTClimb/len(dataLists): ' + str(holdTClimb/len(dataLists)), file=sys.stderr)
+    print ('holdAuto/len(dataLists): ' + str(holdAuto/len(dataLists)), file=sys.stderr)
+    print ('holdAfuel/len(dataLists): ' + str(holdAfuel/len(dataLists)), file=sys.stderr)
+    print ('holdTele/len(dataLists): ' + str(holdTele/len(dataLists)), file=sys.stderr)
+    print ('(1000*holdFuel/holdTotal)//10: ' + str((1000*holdFuel/holdTotal)//10), file=sys.stderr)
+    print ('(1000*holdClimb/holdTotal)//10: ' + str((1000*holdClimb/holdTotal)//10), file=sys.stderr)
+    print ('((1000*holdAuto/holdTotal))//10: ' + str(((1000*holdAuto/holdTotal))//10), file=sys.stderr)
+    print ('((1000*holdTele/holdTotal))//10: ' + str(((1000*holdTele/holdTotal))//10), file=sys.stderr)
 Main ()
 print (json.dumps(finArray))
