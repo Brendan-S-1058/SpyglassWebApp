6e989e3319e5599e8f16064b6c25d6c9fb47f9a2
Brendan-S-1058 <enpassantNH@gmail.com>
2026-01-26 23:37:18 +0000

    wait shoot I should try this one more time actually

--- src/pitFiles.py
+++ src/pitFiles.py
@@ -6,6 +6,6 @@ def Main ():
     inputR = sys.stdin.read()
     inputS = json.loads(inputR)
 
-    print ('inputR: ' + str(inputR), file=sys.stderr)
+    print ('inputS: ' + str(inputS), file=sys.stderr)
 
 Main ()
