4e56769418cbaa84797a52ca95c44f0f0299639e
Brendan-S-1058 <enpassantNH@gmail.com>
2026-02-01 16:57:15 +0000

    more prints, probably need one more actually

--- src/pitFiles.py
+++ src/pitFiles.py
@@ -1,12 +1,30 @@
 import sys
 import json
 import os
+import base64
 
 def Main ():
     inputR = sys.stdin.read()
     inputS = json.loads(inputR)
 
-    print ('inputR: ' + str(inputR), file=sys.stderr)
+    list = []
+    hold = ''
+    for char in inputS:
+        if char != ',':
+            hold += char
+        else:
+            list.append(hold)
+            hold = ''
+    
+    #need to remove the prefix to the link, separated by a comma naturally, very conveinently
+    list.pop(2)
+
+    image_bytes = base64.b64decode(inputS)
+
+
+
+
+    print ('image_bytes: ' + str(image_bytes), file=sys.stderr)
     print ('inputS: ' + str(inputS), file=sys.stderr)
 
 Main ()
